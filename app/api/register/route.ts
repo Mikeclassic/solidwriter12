741dffc5616f247a2aabe00a95b438fa7ebebdc3
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-12-01 21:29:47 +0300

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,1 +1,27 @@
-gg
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const { email, password, name } = await req.json();
+
+    if (!email || !password) {
+      return new NextResponse("Missing info", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await db.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return new NextResponse("User already exists", { status: 500 });
+  }
+}
